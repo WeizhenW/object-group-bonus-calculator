b4aa3a5430126c0c25f98cdd834763881f759c9e
Weizhen Wang <wangwzhen@gmail.com>
2019-05-14 21:48:38 -0500

    basic requirement completed

--- client.js
+++ client.js
@@ -40,4 +40,61 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+//looping through the employees array
+
+for(let i = 0; i < employees.length; i ++) {
+  bonusCalculator(employees[i]); //call the function by passing each individual employee object as argument
+}
+
+//define the function
+function bonusCalculator(employee) {
+
+  let bonusPercentage = 0;
+
+  //check the employee review rating and determine the 1st part of the bonus %
+  if(employee.reviewRating <= 2) {
+    bonusPercentage = 0;
+  } else if(employee.reviewRating === 3) {
+    bonusPercentage = 0.04;
+  } else if(employee.reviewRating === 4) {
+     bonusPercentage = 0.06;
+  } else if (employee.reviewRating === 5) {
+     bonusPercentage = 0.10;
+  }
+
+  //check if tenure > 15y to determine the 2nd part of the bonus %
+  if(employee.employeeNumber.length === 4) {
+    bonusPercentage += 0.05;
+  }
+
+  //check annual income to determine the 3rd part of the bonus %
+  if(employee.annualSalary > 65000) {
+    bonusPercentage -= 0.01;    
+  }
+
+  //check bonus % not higher than 13% and not lower than 0
+  if(bonusPercentage > 0.13) {
+    bonusPercentage = 0.13;
+  } else if(bonusPercentage < 0) {
+    bonusPercentage = 0;
+  }
+
+  //create an empty object to return the result
+
+  let newObj = {};
+
+  //add key/value pairs into the object
+
+  newObj.name = employee.name;
+  newObj.bonusPercentage = bonusPercentage;
+  newObj.totalBonus = Math.round(employee.annualSalary * bonusPercentage);
+  newObj.totalCompensation = Number(employee.annualSalary) + newObj.totalBonus; //annualSalary is string from original array
+
+  //return result
+  console.log(newObj);
+  
+}
+
+
+
